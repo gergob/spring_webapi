9720e79bbcb3f937bd4acde6ec937fe692783239
gergob <greg@grelution.com>
2015-01-28 22:12:10 +0100

    added some extra methods to Controller

--- contact_cards/src/main/java/contact_cards/repositories/ContactController.java
+++ contact_cards/src/main/java/contact_cards/repositories/ContactController.java
@@ -1,11 +1,12 @@
 package contact_cards.repositories;
 
-
 import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import contact_cards.models.Contact;
@@ -13,26 +14,79 @@ import contact_cards.models.Contact;
 @RestController
 public class ContactController {
 
-		@Autowired
-		private ContactRepository repository;
+	@Autowired
+	private ContactRepository repository;
+
+	@RequestMapping("/contacts")
+	public List<Contact> getAllContacts() {
+		List<Contact> result = new LinkedList<Contact>();
+
+		result = repository.findAll();
+
+		return result;
+	}
+
+	@RequestMapping("/contacts/first_name")
+	public List<Contact> getAllByFirstName(
+			@RequestParam(value="value", required=false) String firstName) {
+		List<Contact> result = new LinkedList<Contact>();
+
+		if (!StringUtils.isEmpty(firstName)) {
+			result = repository.findByFirstName(firstName);
+		} else {
+
+			result = repository.findAll();
+		}
+
+		return result;
+	}
+
+	@RequestMapping("/contacts/website")
+	public List<Contact> getContactsByWebsite(
+			@RequestParam(value="value", required=false) String website) {
+		List<Contact> result = new LinkedList<Contact>();
+
+		if (!StringUtils.isEmpty(website)) {
+			result = repository.findByWebsite(website);
+		}
+		else {
+			result = repository.findByWebsite("");
+			result.addAll(repository.findByWebsite(null));
+		}
+
+		return result;
+	}
 	
+	@RequestMapping("/contacts/email")
+	public List<Contact> getContactsByEmail(
+			@RequestParam(value="value", required=false) String email) {
+		List<Contact> result = new LinkedList<Contact>();
+
+		if (!StringUtils.isEmpty(email)) {
+			result = repository.findByEmail(email);
+		}
+		else {
+			result = repository.findByEmail("");
+			result.addAll(repository.findByEmail(null));
+		}
+
+		return result;
+	}
 	
-	 	@RequestMapping("/contacts")
-	    public List<Contact> getAllContacts() {
-	    	List<Contact> result = new LinkedList<Contact>();
-	 		
-	    	result = repository.findAll();
-	    	
-	 		return result;
-	    }
-	 	
-	 	@RequestMapping("/contacts/website")
-	    public List<Contact> getContactsByWebsite() {
-	    	List<Contact> result = new LinkedList<Contact>();
-	 		
-	    	result = repository.findByWebsite("http://tomj.com");
-	    	
-	 		return result;
-	    }
-	
+	@RequestMapping("/contacts/mobile_phone")
+	public List<Contact> getContactsByMobilePhone(
+			@RequestParam(value="value", required=false) String mobilePhone) {
+		List<Contact> result = new LinkedList<Contact>();
+
+		if (!StringUtils.isEmpty(mobilePhone)) {
+			result = repository.findByMobilePhone(mobilePhone);
+		}
+		else {
+			result = repository.findByMobilePhone("");
+			result.addAll(repository.findByMobilePhone(null));
+		}
+
+		return result;
+	}
+
 }
--- contact_cards/src/main/java/contact_cards/repositories/ContactRepository.java
+++ contact_cards/src/main/java/contact_cards/repositories/ContactRepository.java
@@ -3,63 +3,17 @@ package contact_cards.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import contact_cards.models.Contact;
 
 
 public interface ContactRepository extends MongoRepository<Contact, String> {
-
-	public List<Contact> findByWebsite(String website);
+	
 	public List<Contact> findByFirstName(String firstName);
-	
-	
+	public List<Contact> findByLastName(String lastName);
+	public List<Contact> findByWebsite(String website);
+	public List<Contact> findByEmail(String email);
+	public List<Contact> findByMobilePhone(String mobilePhone);
 	
 }
 
-
-
-//
-//import java.net.UnknownHostException;
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//import org.apache.log4j.Logger;
-//
-//import com.mongodb.DB;
-//import com.mongodb.DBCollection;
-//import com.mongodb.MongoClient;
-//import com.mongodb.MongoException;
-//
-//import contact_cards.models.Contact;
-//
-//public class ContactRepository {
-//
-//	private final Logger logger = Logger.getLogger(ContactRepository.class);
-//	
-//	private MongoClient client = null;
-//	private DB database = null;
-//	private DBCollection collection = null;
-//
-//	public ContactRepository() {
-//		try {
-//			this.client = new MongoClient("localhost", 27017);
-//			this.database = client.getDB("contact_db");
-//			this.collection = this.database.getCollection("contacts");
-//		} catch (UnknownHostException unknownHostEx) {
-//			logger.error(unknownHostEx.getMessage(), unknownHostEx);
-//		}
-//		catch(MongoException mongoEx){
-//			logger.error(mongoEx.getMessage(), mongoEx);
-//		}
-//	}
-//
-//	public List<Contact> getAllContacts() {
-//		List<Contact> result = new LinkedList<Contact>();
-//		
-//		DbCursor cursor = this.collection.find();
-//		
-//		return result;
-//	}
-//
-//}
